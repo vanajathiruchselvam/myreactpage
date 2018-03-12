95913cd8739c1d1e2d2f6ff6089890eb906e4566
tvanaja <thiruvanaja964@gmail.com>
2018-03-12 15:32:48 +0530

    new

---
+++ src/common/about.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render(){
+        return (<h1>About Us!!!<br></br>
+
+        0212261847<br></br><br></br></h1>);
+
+    }
+}
+
+export default About
---
+++ src/common/main.component.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">Tasty Cakes</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                {/* Change from a to Link */}
+                                <li><Link to="/" activeclassname="active">Home</Link></li>
+                                <li><Link to="/cars" activeclassname="active">Cakes</Link></li>
+                                <li><Link to="/about" activeclassname="active">About</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main
